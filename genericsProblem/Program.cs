ed0b754c192815593d9d2cff79c905d02f43a8ab
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-01-31 09:37:56 -0300

    Add project files.

---
+++ genericsProblem/Program.cs
@@ -0,0 +1,6 @@
+﻿/*Uma empresa de consultoria deseja avaliar a performance de produtos,
+funcionários, dentre outras coisas. Um dos cálculos que ela precisa é encontrar
+o maior dentre um conjunto de elementos. Fazer um programa que leia um
+conjunto de N produtos, conforme exemplo, e depois mostre o mais caro deles.
+*/
+
