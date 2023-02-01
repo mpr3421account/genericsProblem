a5c75b2abf551be1825660cda229e12f74392dfd
Marcos Ribeiro <marcos@azacessorios.com.br>
2023-02-01 08:29:23 -0300

    Método Genérico T (restrições para generics)

--- genericsProblem/Services/CalculationService.cs
+++ genericsProblem/Services/CalculationService.cs
@@ -8,18 +8,18 @@ namespace genericsProblem.Services
 {
     internal class CalculationService
     {
-        public int Max(List<int> list)
+        public T Max<T>(List<T> list) where T : IComparable//método com o tipo T genérico precisa ser comparável (IComparable Interface)
         {
             if (list.Count == 0)
             {
                 throw new ArgumentException("The list can not be empty");
             }
 
-            int max = list[0];
+            T max = list[0];
                 {
                 for (int i = 1; i < list.Count; i++)
 
-                    if (list[i] > max)
+                    if (list[i].CompareTo(max) > 0)//operação genérica tipo T utilizando a interface IComparable
                     {
                         max = list[i];
                     }
